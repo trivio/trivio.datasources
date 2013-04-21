582f965d3fbda4b1c9fd39f7aa36da68af138bcf
Scott Robertson <srobertson@codeit.com>
2013-04-20 20:57:36 -0700

    sort modutils results for before comparing

--- tests/test_modutils.py
+++ tests/test_modutils.py
@@ -7,7 +7,7 @@ class TestModUtils(TestCase):
     self.path = os.path.join(os.path.dirname(__file__), 'data/fake_python_package')
     
   def test_modules_from_path(self):
-    modules = list(modutils.modules_from_path(self.path))
+    modules = sorted(modutils.modules_from_path(self.path))
     self.assertSequenceEqual(
       modules,
       ['a','b','subpackage.c']
