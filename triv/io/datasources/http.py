7cf7a6b0c41674faf9dd07251af2533313e0a91e
Scott Robertson <srobertson@codeit.com>
2013-04-20 20:51:31 -0700

    Only set content type if it hasn't ben explicitly set.

--- triv/io/datasources/http.py
+++ triv/io/datasources/http.py
@@ -10,7 +10,8 @@ class HTTPSource(datasources.DataSource):
   def input_stream(stream, size, url, params):
     stream, size, url = http_input_stream(stream, size,url,params)
     params.headers = stream.headers
-    params.content_type = stream.headers['content-type']
+    if not hasattr(params, 'content_type'):
+      params.content_type = stream.headers['content-type']
     return stream
     
   def earliest_record_time(self):
