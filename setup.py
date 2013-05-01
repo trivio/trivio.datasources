3c141ec87b06bdb74e4a9a4fccb775edf65c55af
Scott Robertson <srobertson@codeit.com>
2013-04-30 19:47:07 -0700

    require python-hadoop

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='triv.io.datasources',
-    version = '0.1',
-    description='Third party datasources for htttp://triv.io',
-    author='Scott Robertson',
-    author_email='srobertson@codeit.com',
-    packages = find_packages(),
-    install_requires = ['boto', 'warc', 'python-dateutil==1.5', 'MySQL-python']
+  name='triv.io.datasources',
+  version = '0.1',
+  description='Third party datasources for htttp://triv.io',
+  author='Scott Robertson',
+  author_email='scott@triv.io',
+  packages = find_packages(),
+
+  dependency_links = [
+    'https://github.com/trivio/Hadoop/tarball/master#egg=Hadoop'
+  ],
+  install_requires = [
+    'boto',
+    'Hadoop',
+    'warc',
+    'python-dateutil==1.5',
+    'MySQL-python'
+  ]
 
 )
